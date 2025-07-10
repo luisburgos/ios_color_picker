f76dc9728bfe8426b37f3630aff6b66282b68d3a
Luis Burgos <luisburgos@users.noreply.github.com>
2025-07-10 14:00:08 -0600

    fix: add lib entry point

---
+++ lib/ios_color_picker.dart
@@ -0,0 +1,7 @@
+library ios_color_picker;
+
+export 'custom_picker/extensions.dart';
+export 'custom_picker/ios_color_picker.dart';
+export 'ios_color_picker.dart';
+export 'native_picker/ios_color_picker.dart';
+export 'show_ios_color_picker.dart';
