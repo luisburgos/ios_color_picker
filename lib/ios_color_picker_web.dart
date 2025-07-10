4d5055ad6b678946ca6e8921b01edc9d5b340e21
Luis Burgos <luisburgos@users.noreply.github.com>
2025-07-10 13:54:36 -0600

    fix: web registration

--- lib/ios_color_picker_web.dart
+++ lib/ios_color_picker_web.dart
@@ -4,7 +4,8 @@
 // ignore: avoid_web_libraries_in_flutter
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'ios_color_picker_platform_interface.dart';
+
+import 'native_picker/ios_color_picker_platform_interface.dart';
 
 /// A web implementation of the IosColorPickerPlatform of the IosColorPicker plugin.
 class IosColorPickerWeb extends IosColorPickerPlatform {
